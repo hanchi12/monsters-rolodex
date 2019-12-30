2a7c5b6e9d4a7eb0ca6e6c59cd45463ce5701a26
Michael Cane <mvcane17@gmail.com>
2019-12-30 09:15:58 +0800

    Updates

---
+++ precache-manifest.9261418a84f16400743c8b0f3c962b84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "714e0aeb356ec9a0629a4db357e1baa7",
+    "url": "/hanchi12/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "db43e26694dc454b8aa9",
+    "url": "/hanchi12/monsters-rolodex/static/css/main.3fd000ce.chunk.css"
+  },
+  {
+    "revision": "98c958d22c877b04bc8e",
+    "url": "/hanchi12/monsters-rolodex/static/js/2.ea62ce66.chunk.js"
+  },
+  {
+    "revision": "7fc60fe721fa046663bd312b1d917655",
+    "url": "/hanchi12/monsters-rolodex/static/js/2.ea62ce66.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db43e26694dc454b8aa9",
+    "url": "/hanchi12/monsters-rolodex/static/js/main.13e86766.chunk.js"
+  },
+  {
+    "revision": "429ce20a2885a35f8cd3",
+    "url": "/hanchi12/monsters-rolodex/static/js/runtime-main.0522cde0.js"
+  }
+]);
